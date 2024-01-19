9b8953fef27dc3579c7afb777c682cbe1e3f3bcc
Muhammad suleman <157087262+suleman6@users.noreply.github.com>
2024-01-19 09:34:41 +0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+// document.getElementById("btn").onclick = function () {
+  
+  
+   function btnfunction () {
+  if (nav.style.display === "none") {
+    nav.style.display = "block"
+    btn.style.display = "none"
+  }
+  else{(nav.style.display = "none") 
+  btn.style.display = "block"
+  }
+};
+
+
+
+
+
+// setInterval(() => {
+//   if (nav.style.display === "none") {
+//     nav.style.display = "block"
+//   }
+//   else{(nav.style.display = "none") 
+//   }
+// }, 1000);
+
